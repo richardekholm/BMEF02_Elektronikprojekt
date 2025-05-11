cd3cad485218ef4a346a1ce9c731a0ce7d8e787f
Mundt-Petersen <walter.mundt.petersen737@gmail.com>
2025-05-11 17:01:29 +0200

    added distance functions, ball detection and dump, compass lib

--- src/mega.cpp
+++ src/mega.cpp
@@ -1,6 +1,8 @@
 #include <Wire.h>
 #include <Arduino.h>
 #include <Servo.h>
+#include <LSM303D.h>
+
 
 #define ARDUINO_ADDRESS 8  // Must match ESP32 sender
 
@@ -20,7 +22,9 @@ const int ledPin = 13;
 const int rightTrigPin = 32,  
           rightEchoPin = 33, 
           backTrigPin = 30,
-          backEchoPin = 31;
+          backEchoPin = 31,
+          bucketTrigPin = 34,
+          bucketEchoPin = 35;
 
 const int servoPinA = A2,
           servoPinB = A3;
@@ -34,7 +38,14 @@ const int turnTime = 100;
 const int stepTime = 100;
 const int stopTime = 500; 
 const int motorSpeed = 90;
-float duration, distance;
+
+int accel[3];  // we'll store the raw acceleration values here
+int mag[3];  // raw magnetometer values stored here
+float realAccel[3];  // calculated acceleration values here
+float heading, titleHeading;
+
+int hits = 0;
+unsigned long startTime = 0;
 
 
 // Declare robot state and timing variables
@@ -53,7 +64,10 @@ void receiveData(int byteCount);
 void test(int duration);
 void search(int duration);
 void moveToNet();
-void dumpBall();
+void setBucketHeight(String height);
+boolean ballInBucket();
+float getInstantDistance(int trigPin, int echoPin);
+float getAvgDistance(int trigPin, int echoPin);
 
 
 
@@ -67,127 +81,148 @@ void lightOff() {
 }
 
 void turnRight(int duration) {
-  Serial.println("Turning right");
-    delay(duration);
-    digitalWrite(BRAKE_A, LOW);    
-    digitalWrite(BRAKE_B, LOW);
-    digitalWrite(DIR_A, HIGH);    
-    digitalWrite(DIR_B, LOW);    
-    analogWrite(PWM_A, motorSpeed);
-    analogWrite(PWM_B, motorSpeed);
-    delay(duration);
-    stop(stopTime);
-  }
-  
-  void moveBackward(int duration) {
-    Serial.println("Moving backward");
-    digitalWrite(BRAKE_A, LOW);
-    digitalWrite(BRAKE_B, LOW);
-    digitalWrite(DIR_A, HIGH);     
-    digitalWrite(DIR_B, HIGH);     
-    analogWrite(PWM_A, motorSpeed);
-    analogWrite(PWM_B, motorSpeed);
-    delay(duration);
-  }
-  
-  void moveForward(int duration) {
-    Serial.println("Moving forward");
-    digitalWrite(BRAKE_A, LOW);    
-    digitalWrite(BRAKE_B, LOW);
-    digitalWrite(DIR_A, LOW);    
-    digitalWrite(DIR_B, LOW);     
-    analogWrite(PWM_A, motorSpeed);
-    analogWrite(PWM_B, motorSpeed);
-    delay(duration);     
-  } 
-  
-  void turnLeft(int duration) {
-    Serial.println("Turning left");
-    delay(duration);
-    digitalWrite(BRAKE_A, LOW);    
-    digitalWrite(BRAKE_B, LOW);
-    digitalWrite(DIR_A, LOW);     
-    digitalWrite(DIR_B, HIGH);    
-    analogWrite(PWM_A, motorSpeed);
-    analogWrite(PWM_B, motorSpeed);
-    delay(duration);
-    stop(stopTime);
+  delay(duration);
+  digitalWrite(BRAKE_A, LOW);    
+  digitalWrite(BRAKE_B, LOW);
+  digitalWrite(DIR_A, HIGH);    
+  digitalWrite(DIR_B, LOW);    
+  analogWrite(PWM_A, motorSpeed);
+  analogWrite(PWM_B, motorSpeed);
+  delay(duration);
+  stop(stopTime);
+}
+
+void moveBackward(int duration) {
+  digitalWrite(BRAKE_A, LOW);
+  digitalWrite(BRAKE_B, LOW);
+  digitalWrite(DIR_A, HIGH);     
+  digitalWrite(DIR_B, HIGH);     
+  analogWrite(PWM_A, motorSpeed);
+  analogWrite(PWM_B, motorSpeed);
+  delay(duration);
+}
+
+void moveForward(int duration) {
+  digitalWrite(BRAKE_A, LOW);    
+  digitalWrite(BRAKE_B, LOW);
+  digitalWrite(DIR_A, LOW);    
+  digitalWrite(DIR_B, LOW);     
+  analogWrite(PWM_A, motorSpeed);
+  analogWrite(PWM_B, motorSpeed);
+  delay(duration);     
+} 
+
+void turnLeft(int duration) {
+  delay(duration);
+  digitalWrite(BRAKE_A, LOW);    
+  digitalWrite(BRAKE_B, LOW);
+  digitalWrite(DIR_A, LOW);     
+  digitalWrite(DIR_B, HIGH);    
+  analogWrite(PWM_A, motorSpeed);
+  analogWrite(PWM_B, motorSpeed);
+  delay(duration);
+  stop(stopTime);
 }
 
 void stop(int duration) {
   digitalWrite(BRAKE_A, HIGH);    
   digitalWrite(BRAKE_B, HIGH);
-    analogWrite(PWM_A, 0);  // Stop motor A
-    analogWrite(PWM_B, 0);  // Stop motor B
-    Serial.println("Stopping motors");
-    delay(duration);
+  analogWrite(PWM_A, 0);  // Stop motor A
+  analogWrite(PWM_B, 0);  // Stop motor B
+  delay(duration);
 }
 
-float getDistance(int trigPin, int echoPin){
-  distance = 0;
-  int nbrOfReadings = 10;
-  for (int i = 0; i < nbrOfReadings; i++) {
-    digitalWrite(trigPin, LOW);  
-    delayMicroseconds(2);  
-    digitalWrite(trigPin, HIGH);  
-    delayMicroseconds(2);  
-    digitalWrite(trigPin, LOW);  
-    duration = pulseIn(echoPin, HIGH);
-    if(distance > 100){
-      nbrOfReadings--;
-      continue;
-    }
-    distance += (duration*.0343)/2;  
-    
+// float getInstantDistance(int trigPin, int echoPin){
+//   float distance = 0;
+//   float duration = 0;
+//   digitalWrite(trigPin, LOW);  
+//     delayMicroseconds(2);  
+//     digitalWrite(trigPin, HIGH);  
+//     delayMicroseconds(10);
+//     digitalWrite(trigPin, LOW);
+//     duration = pulseIn(echoPin, HIGH);
+//     distance = (duration*.0343)/2;
+//     return distance;
+// }
+
+float getInstantDistance(int trigPin, int echoPin) {
+  // fire pulse
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2);  
+  digitalWrite(trigPin, HIGH);  
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // measure time
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  if (duration == 0 || duration > 25000) {
+    return -1.0f;     // signal “bad reading”
   }
-  if (nbrOfReadings < 1){
-    return 6969;
-  }
-  return distance/nbrOfReadings;
+  // convert to cm
+  return (duration * 0.0343f) / 2.0f;
 }
 
 void search(int duration) {
-  Serial.println("Searching...");
   // delay(stepTime);
-  turnRight(stepTime);
+  turnRight(stepTime); 
   // stop(duration*2);
   // delay(stepTime*2);
 }
 
-void dumpBall(){
-  servoA.write(110);      // Move to 0 degrees
-  servoB.write(10);      // Move to 0 degrees
-  Serial.println("Moving to 0 degrees");
-  delay(5000);
-  // servoA.write(30);     // Move to 90 degrees
-  // servoB.write(90);     // Move to 90 degrees
-  // Serial.println("Moving to 90 degrees");
-  // delay(5000);
-  servoA.write(55);    // Move to 180 degrees
-  servoB.write(65);    // Move to 180 degrees
-  delay(1000);
-  servoA.write(0);    // Move to 180 degrees
-  servoB.write(120);    // Move to 180 degrees
-  Serial.println("Moving to 120 degrees");
-  delay(5000);
+void setBucketHeight(String height){
+  if(height == "LOW"){
+    servoA.write(110);    // Move to 0 degrees
+    servoB.write(10);     // Move to 0 degrees
+    Serial.println("Moving to 10 degrees");
+    delay(1000); //alltså sänka denna?
+  }
+  else if(height == "MID"){
+    servoA.write(55);     // Move to 55 degrees
+    servoB.write(65);     // Move to 55 degrees
+    delay(1000);
+    Serial.println("Moving to 55 degrees");
+  }
+  else if(height == "HI"){
+    servoA.write(0);      // Move to 120 degrees
+    servoB.write(120);    // Move to 120 degrees
+    Serial.println("Moving to 90 degrees");
+    delay(1000);
+  }
+}
+
+boolean ballInBucket(){
+  float dis = getInstantDistance(bucketTrigPin, bucketEchoPin);
+  Serial.println(dis);
+  
+  if (dis < 14){
+    hits++;
+  }
+
+  unsigned long now = millis();
+  if (now - startTime >= 500UL) {
+    if (hits > 30) {
+      Serial.println("Ball detected.");
+    }
+    hits = 0;
+    startTime = now;
+  }
 }
 
 void receiveData(int byteCount) {
   if (byteCount < 10){
     return; 
   }
-    
-
+  
   int target = Wire.read();
   int score  = Wire.read();
-
+  
   int x = (Wire.read() << 8) | Wire.read();
   int y = (Wire.read() << 8) | Wire.read();
   int w = (Wire.read() << 8) | Wire.read();
   int h = (Wire.read() << 8) | Wire.read();
-
-  int midX = x + (w / 2);
-
+  
+  int midX = x + (w / 2); // vi ska testa  om detta är the case eller om x redan returneras som mittpunkt från ESP32
+  
   // Serial.println(w);
   if (midX <= 80) {
     robotState = LEFT;
@@ -205,52 +240,57 @@ void receiveData(int byteCount) {
 
 
 void moveToNet(){
-  //Lägg till liten elevering av skopan i syfte behålla bollen i skopan
-  float rightDistance = getDistance(rightTrigPin, rightEchoPin);
-  float backDistance = getDistance(backTrigPin, backEchoPin);
-  float shortDistance = 4.5;
-  float midDistance = 8; //Beror på pinnens längd
-  while(backDistance > shortDistance){
-    Serial.print("rightDistance = ");
-    Serial.println(rightDistance);
-    Serial.print("backDistance = ");
-    Serial.println(backDistance);
+  // setBucketHeight("MID"); //Bucket in transport mode 
+  
+  // float rightDistance = getAvgDistance(rightTrigPin, rightEchoPin);
+  // float backDistance = getAvgDistance(backTrigPin, backEchoPin);
+  // float shortDistance = 4.5;
+  // float midDistance = 8; //Beror på pinnens längd
+  // while(backDistance > shortDistance){
+  //   Serial.print("rightDistance = ");
+  //   Serial.println(rightDistance);
+  //   Serial.print("backDistance = ");
+  //   Serial.println(backDistance);
     
-    if(backDistance > midDistance && rightDistance > midDistance){ //CASE 1: Center of arena (Find wall to begin traversing)
-      Serial.println("case 1");
-      moveBackward(stepTime);
-    }
-    else if(backDistance < midDistance && backDistance > shortDistance && rightDistance > midDistance){ //CASE 2: Back at wall, not parallell to wall (rotate until parallell)
-      Serial.println("case 2");
-      moveBackward(stepTime);
-      while (rightDistance > midDistance){
-        rightDistance = getDistance(rightTrigPin, rightEchoPin);
-        moveForward(stepTime);
-        turnRight(stepTime*2);
-      }
-    }
-    else if(backDistance > midDistance && rightDistance < midDistance){ //CASE 2,5: Back at wall, parallell to wall (Move along wall)
-      Serial.println("case 2.5");
-      if (rightDistance < 2*shortDistance){
-        turnRight(stepTime*2);
-        // continue;
-      }
-      moveBackward(stepTime);
-    }
-    else if(backDistance < midDistance && backDistance > shortDistance && rightDistance < midDistance){ //CASE 3: Corner (Rotate until parallell again)
-      Serial.println("case 3");
-      turnRight(stepTime);
-    }
+  //   if(backDistance > midDistance && rightDistance > midDistance){ //CASE 1: Center of arena (Find wall to begin traversing)
+  //     Serial.println("case 1");
+  //     moveBackward(stepTime);
+  //   }
+  //   else if(backDistance < midDistance && backDistance > shortDistance && rightDistance > midDistance){ //CASE 2: Back at wall, not parallell to wall (rotate until parallell)
+  //     Serial.println("case 2");
+  //     moveBackward(stepTime/2);
+  //     backDistance = getAvgDistance(backTrigPin, backEchoPin);
+  //     if (backDistance < shortDistance){
+  //       break;
+  //     }
+  //     moveForward(stepTime);
+  //     while (rightDistance > midDistance){
+  //       rightDistance = getAvgDistance(rightTrigPin, rightEchoPin);
+  //       turnRight(stepTime*2);
+  //     }
+  //   }
+  //   else if(backDistance > midDistance && rightDistance < midDistance){ //CASE 2,5: Back at wall, parallell to wall (Move along wall)
+  //     Serial.println("case 2.5");
+  //     if (rightDistance < 2*shortDistance){
+  //       turnRight(stepTime*2);
+  //       // continue;
+  //     }
+  //     moveBackward(stepTime);
+  //   }
+  //   else if(backDistance < midDistance && backDistance > shortDistance && rightDistance < midDistance){ //CASE 3: Corner (Rotate until parallell again)
+  //     Serial.println("case 3");
+  //     turnRight(stepTime);
+  //   }
     
-    rightDistance = getDistance(rightTrigPin, rightEchoPin);
-    backDistance = getDistance(backTrigPin, backEchoPin);
-    delay(stepTime);
-  }
-  Serial.println("case 4");
-  Serial.println("Net found.");
-  robotState = DUMP;
-  // robotState = SEARCH;
+  //   rightDistance = getAvgDistance(rightTrigPin, rightEchoPin);
+  //   backDistance = getAvgDistance(backTrigPin, backEchoPin);
+  //   delay(stepTime);
+  // }
+  // Serial.println("case 4");
+  // Serial.println("Net found.");
+  // robotState = DUMP;
 }
+
 
 void setup() {
   Wire.begin(ARDUINO_ADDRESS);  // Join I2C bus as slave
@@ -259,49 +299,97 @@ void setup() {
 	pinMode(rightEchoPin, INPUT);
   pinMode(backTrigPin, OUTPUT);  
 	pinMode(backEchoPin, INPUT);
-  
+	pinMode(bucketEchoPin, INPUT);
+  pinMode(bucketTrigPin, OUTPUT);  
+  char rtn = 0;
+  rtn = Lsm303d.initI2C();
+
   servoA.attach(servoPinA);  // attach to digital pin 5
   servoB.attach(servoPinB);  // attach to digital pin 6
-
+  setBucketHeight("LOW");
   Serial.begin(9600);
+
+  startTime = millis();
 }
+
+boolean distanceTrig(int trigPin, int echoPin, int nbrOfHits, float threshold){
+  int hitStreak = 0;
+  for(int i = 0; i < nbrOfHits; i++){
+    float dist = getInstantDistance(trigPin, echoPin);
+    Serial.println(dist);
+    if(dist < threshold){
+      hitStreak ++;
+    }
+  }
+  Serial.println(hitStreak);
+  if (hitStreak == nbrOfHits){
+    return true;
+  }
+  return false;
+} 
 
 void loop() {
-  moveToNet();
-  if ((millis() - lastMoveTime) > moveDuration && robotState != SEARCH) {
-    // Serial.println("moveduration");
-    // stop();
-    //robotState = SEARCH;
-    // Serial.println("Searching...");
-    // robotState = RIGHT;
-  return;
-}
-// robotState = IDLE;  // Reset state to IDLE after each loop iteration
+
+  
+  if(distanceTrig(bucketTrigPin, bucketEchoPin, 10, 14)){ //Boll i skopa
+    Serial.println("Ball detected.");
+    robotState = DUMP;
+  }
+
+
+  
+	Lsm303d.getAccel(accel);
+	while(!Lsm303d.isMagReady());
+	Lsm303d.getMag(mag);  
+	
+	for (int i=0; i<3; i++)
+	{
+		realAccel[i] = accel[i] / pow(2, 15) * ACCELE_SCALE;  // calculate real acceleration values, in units of g
+	}
+	heading = Lsm303d.getHeading(mag);
+	titleHeading = Lsm303d.getTiltHeading(mag, realAccel);
+	
+  Serial.println(titleHeading, 3); //printar riktning i grader
+  
+  
+
   switch (robotState) {
     case FORWARD:
+      Serial.println("Moving forward");
       moveForward(moveTime); 
       break;
     case BACKWARD:
+      Serial.println("Moving backward");
       moveBackward(moveTime); 
       break;
     case LEFT:
+      Serial.println("Turning left");
       turnLeft(stepTime); 
       break;
     case RIGHT:
+      Serial.println("Turning right");
       turnRight(stepTime); 
       break;
     case STOP:
+      Serial.println("Stopping motors");
       stop(stopTime);
       break;
     case SEARCH:
+      Serial.println("Searching...");
       search(stepTime);
       default:
       break;
     case TO_NET:
       moveToNet();
+      robotState = DUMP;
       break;
     case DUMP:
-      dumpBall();
+      Serial.println("DUMPING");
+      setBucketHeight("MID");
+      setBucketHeight("HI");
+      setBucketHeight("LOW");
+      delay(500);
+      robotState = SEARCH;
       break;
     case IDLE:
       break;
